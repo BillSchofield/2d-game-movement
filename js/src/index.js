55b5b515dd25ef7f4e87ff09b79c8b18d84532da
Bill Schofield <bill@billschofield.net>
2026-08-26 17:03:11 -0500

    Broke out vector2d class.

--- js/src/index.js
+++ js/src/index.js
@@ -1,27 +1,22 @@
+"use strict";
+
 const canvas = document.getElementById('myCanvas');
 const context = canvas.getContext("2d");
 
 let currentInterval;
 let image = new Image();
 image.src = './images/rocket-ship.png';
-var rotation = Math.PI/8;
+
 const spaceShip = new Entity(
     image,
-    (canvas.width - 300) / 2,
-    (canvas.height - 300) / 2,
-    rotation,
+    new Vector2d((canvas.width - 300) / 2,(canvas.height - 300) / 2),
+    Math.PI/8,
     1
     );
 
-update(image);
-function drawImage(image, x, y, scale, r){
-    context.setTransform(scale, 0, 0, scale, x, y);
-    context.rotate(r);
-    context.drawImage(image, -image.width / 2, -image.height / 2);
-    context.setTransform(1,0,0,1,0,0);
-}
+start();
 
-function update(image) {
+function start() {
     currentInterval = setInterval(function () {
         context.clearRect(0, 0, canvas.width, canvas.height);
         spaceShip.update();
